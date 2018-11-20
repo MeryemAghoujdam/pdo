5ca4e02e8b1be291edc4b40c9b5139c2aa8df1ed
watashiwa L desu <watashiwa L desu@DESKTOP-RVGD8P5>
2018-11-20 16:13:58 +0100

    My Class

--- service/MatiereService.php
+++ service/MatiereService.php
@@ -14,13 +14,13 @@
 //chdir("../");
 include_once 'beans/Matiere.php';
 include_once 'dao/IDao.php';
-include_once 'connexion/MatiereConnexion.php';
+include_once 'connexion/Connexion.php';
 
 class MatiereService implements IDao {
     //put your code here
     private $cnx;
     function __construct() {
-        $this->cnx = new MatiereConnexion();
+        $this->cnx = new Connexion();
     }
 
     public function create($o) {
