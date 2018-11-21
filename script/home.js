085fecfdb3e3e96b4f0d803a80d4d30314642cf4
iliaselbadaoui <iliassovich@outlook.com>
2018-11-21 23:39:47 +0100

    Commit FontAwesome

---
+++ script/home.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function showMenu(){
+    var element = document.getElementById("menuBlock");
+    element.style.height=(window.innerHeight-64)+'px';
+}
